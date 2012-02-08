b1bab39270e306b459df5378a3aa291307fe554c
Gethryn Ghavalas <gethryn@mac.com>
2012-02-08 19:01:37 +1100

    Initial Pages created, erlang excel done

--- app.rb
+++ app.rb
@@ -7,3 +7,19 @@ get '/' do
 	haml :index
 end
 
+get '/iphone/erlang' do
+  haml :erlang_iphone
+end
+
+get '/excel/forecast' do
+  haml :forecast
+end
+
+get '/excel/shrinkage' do
+  haml :shrinkage
+end
+
+get '/excel/erlang' do
+  haml :erlang_excel
+end
+
