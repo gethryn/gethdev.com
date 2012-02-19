d6e7858a9f9adb3175d398820c24af865d84076b
Gethryn Ghavalas <gethryn@mac.com>
2012-02-19 15:12:43 +1100

    streamlined BOOTSTRAP, added form, reduced graph size

--- app.rb
+++ app.rb
@@ -2,24 +2,34 @@
 require 'sinatra'
 require 'haml'
 
+# set UTF-8 for outgoing
+before do
+  headers "Content-Type" => "text/html; charset=utf8"
+end
 
 get '/' do
+  @title = "Welcome to gethdev.com"
 	haml :index
 end
 
 get '/iphone/erlang' do
+  @title = "gethdev.com - ErlangC v1.1 for iPhone"
   haml :erlang_iphone
 end
 
-get '/excel/forecast' do
-  haml :forecast
-end
-
-get '/excel/shrinkage' do
-  haml :shrinkage
-end
-
 get '/excel/erlang' do
+  @title = "gethdev.com - Performing ErlangC Calculations in Excel"
   haml :erlang_excel
 end
 
+get '/support' do
+  @title = "gethdev.com Support"
+  haml :support
+end
+
+post '/support' do
+    @name = "#{params[:post][:name]}"
+    @email = "#{params[:post][:email]}"
+    @issue = "#{params[:post][:issue]}"
+    haml :support
+end
